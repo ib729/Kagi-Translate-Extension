f0d04a88425de943d710166c0e1f0796f181a655
Ivan Belousov <105873927+ib729@users.noreply.github.com>
2025-08-05 18:02:22 +0800

    Commit extension code

--- background.js
+++ background.js
@@ -1,15 +1,29 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: "kagi-translate",
-    title: "Kagi Translate",
-    contexts: ["selection"],
+    id: "kagi-translate-standard",
+    title: "Kagi Translate (Standard)",
+    contexts: ["selection"]
+  });
+  chrome.contextMenus.create({
+    id: "kagi-translate-best",
+    title: "Kagi Translate (Best)",
+    contexts: ["selection"]
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "kagi-translate" && info.selectionText) {
+  if (info.selectionText) {
     const selectedText = encodeURIComponent(info.selectionText);
-    const translateUrl = `https://translate.kagi.com/?source=&target=en&text=${selectedText}`;
-    chrome.tabs.create({ url: translateUrl });
+    let translateUrl;
+
+    if (info.menuItemId === "kagi-translate-standard") {
+      translateUrl = `https://translate.kagi.com/?source=&target=en&text=${selectedText}`;
+    } else if (info.menuItemId === "kagi-translate-best") {
+      translateUrl = `https://translate.kagi.com/?source=&target=en&text=${selectedText}&from=auto&to=en&quality=best`;
+    }
+
+    if (translateUrl) {
+      chrome.tabs.create({ url: translateUrl });
+    }
   }
 });
